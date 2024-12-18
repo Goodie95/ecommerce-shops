a3648876a13d647c07bba6ad557cbb0229096e94
Goodie95 <djlangra95@gmail.com>
2024-12-19 01:05:31 +0300

    Added submodule ecommerce_website

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scans all files in src directory
+    "./public/index.html",        // Includes the main HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
